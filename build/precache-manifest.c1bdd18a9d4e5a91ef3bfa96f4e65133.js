f03de019675f3dba4ba3c2b3185d4874633622d5
Cesar Gamboa <ff_cesar@FF-Admins-MacBook-Pro.local>
2020-12-05 15:11:50 -0600

    test

---
+++ build/precache-manifest.c1bdd18a9d4e5a91ef3bfa96f4e65133.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae7558c2973e2887e473530eccec33d",
+    "url": "/s/new/index.html"
+  },
+  {
+    "revision": "74f446157853e973186c",
+    "url": "/s/new/static/css/main.05dad1b8.chunk.css"
+  },
+  {
+    "revision": "e6551b6e34d1b8d07b05",
+    "url": "/s/new/static/js/2.dba4e604.chunk.js"
+  },
+  {
+    "revision": "74f446157853e973186c",
+    "url": "/s/new/static/js/main.3b5443f0.chunk.js"
+  },
+  {
+    "revision": "85945ddbca995500257a",
+    "url": "/s/new/static/js/runtime~main.01695a31.js"
+  },
+  {
+    "revision": "049e64cd4c92ad6cabe65cd834e63c78",
+    "url": "/s/new/static/media/1.049e64cd.jpg"
+  },
+  {
+    "revision": "27e103909c2ae38a070c8304aca2dec1",
+    "url": "/s/new/static/media/2.27e10390.jpg"
+  },
+  {
+    "revision": "f074fa5b5c1f02a72955c1ce2917e7ee",
+    "url": "/s/new/static/media/3.f074fa5b.jpg"
+  },
+  {
+    "revision": "eaa43e13ac43cc06779709216cab33e6",
+    "url": "/s/new/static/media/4.eaa43e13.jpg"
+  },
+  {
+    "revision": "d4896665cecb227b5835dc30e67355c6",
+    "url": "/s/new/static/media/5.d4896665.jpg"
+  }
+]);
